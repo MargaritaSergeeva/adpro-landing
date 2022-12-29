a5c23499c787627eebaba05a6493a1d668941eb2
Margarita Sergeeva <margaritk@inbox.ru>
2022-12-29 14:21:42 +0300

    added countries and targeting section

---
+++ src/components/CountriesSlider.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="countries-slider">
+    <swiper
+      class="countries-slider__slider"
+      :modules="[Navigation]"
+      :speed="500"
+      :navigation="{
+        nextEl: '.countries-slider__next',
+        prevEl: '.countries-slider__prev'
+      }"
+      :breakpoints="{
+      '360': {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+      '768': {
+        slidesPerView: 6,
+        spaceBetween: 25,
+      },
+      '1300': {
+        slidesPerView: 8,
+        spaceBetween: 35,
+      },
+    }"
+      loop
+    >
+      <swiper-slide v-for="country in countriesData" :key="country.id" class="countries-slider__slide">
+        <q-card class="countries-slider__card">
+          <img
+            class="countries-slider__img"
+            :src="'flags/' + country.img + '.svg'"
+            :alt="country.img">
+        </q-card>
+      </swiper-slide>
+    </swiper>
+    <div class="countries-slider__prev"></div>
+    <div class="countries-slider__next"></div>
+  </div>
+
+
+</template>
+
+<script setup lang="ts">
+import {Navigation} from 'swiper';
+import {Swiper, SwiperSlide} from 'swiper/vue';
+import AOS from 'aos';
+import {onMounted} from 'vue';
+
+onMounted(() => {
+  AOS.init();
+})
+
+const countriesData = [
+  {
+    id: 1,
+    img: 'id',
+  },
+  {
+    id: 2,
+    img: 'de',
+  },
+  {
+    id: 3,
+    img: 'my',
+  },
+  {
+    id: 4,
+    img: 'am',
+  },
+  {
+    id: 5,
+    img: 'th',
+  },
+  {
+    id: 6,
+    img: 'ua',
+  },
+  {
+    id: 7,
+    img: 'kaz',
+  },
+  {
+    id: 8,
+    img: 'lt',
+  },
+  {
+    id: 9,
+    img: 'ph',
+  },
+  {
+    id: 10,
+    img: 'cz',
+  },
+  {
+    id: 11,
+    img: 'it',
+  }, {
+    id: 12,
+    img: 'ro',
+  }, {
+    id: 13,
+    img: 'in',
+  }, {
+    id: 14,
+    img: 'vn',
+  },
+  {
+    id: 15,
+    img: 'ee',
+  },
+  {
+    id: 16,
+    img: 'es',
+  },
+];
+</script>
--- src/pages/PubsPage.vue
+++ src/pages/PubsPage.vue
@@ -1,31 +1,77 @@
 <template>
   <q-page class="pubs-page">
     <section class="pubs-page__main-section pubs-main-section ">
-    <div class="pubs-main-section__container container">
-      <div class="pubs-main-section__wrapper">
-        <div class="pubs-main-section__text-block">
-          <h1 class="custom-font pubs-main-section__title page-title">Earn more with AdProfex ad network</h1>
-          <p class="pubs-main-section__text">We provide opportunities for simple and profitable monetization of a site of any size.</p>
-          <p class="pubs-main-section__text">We are pleased to collaborate with both large publishers and owners of small sites.</p>
-        </div>
-        <div class="pubs-main-section__img-block">
-          <div class="pubs-main-section__img-block-bg">
-            <img class="pubs-main-section__img-block-figure1" src="~assets/pubs/pubs-ellipse1.png" srcset="~assets/pubs/pubs-ellipse1.png 1x, ~assets/pubs/pubs-ellipse1-2x.png 2x" alt="ellipse1">
-            <img class="pubs-main-section__img-block-figure2" src="~assets/pubs/pubs-ellipse2.png" srcset="~assets/pubs/pubs-ellipse2.png 1x, ~assets/pubs/pubs-ellipse2-2x.png 2x" alt="ellipse2">
-            <img class="pubs-main-section__img-block-figure3" src="~assets/pubs/pubs-rectangle1.png" srcset="~assets/pubs/pubs-rectangle1.png 1x, ~assets/pubs/pubs-rectangle1-2x.png 2x" alt="rectangle-3">
-            <img class="pubs-main-section__img-block-figure4" src="~assets/pubs/pubs-rectangle2.svg" alt="rectangle2">
-            <img class="pubs-main-section__img-block-figure5" src="~assets/pubs/pubs-rectangle1.svg" alt="rectangle1">
-            <img class="pubs-main-section__img-block-figure6" src="~assets/pubs/pubs-ellipse.svg" alt="ellipse1">
-            <img class="pubs-main-section__img-block-figure7" src="~assets/pubs/tisers.png" srcset="~assets/pubs/tisers.png 1x, ~assets/pubs/tisers-2x.png 2x" alt="rectangle3">
+      <div class="pubs-main-section__container container">
+        <div class="pubs-main-section__wrapper">
+          <div class="pubs-main-section__text-block">
+            <h1 class="custom-font pubs-main-section__title page-title">Earn more with AdProfex ad network</h1>
+            <p class="pubs-main-section__text">We provide opportunities for simple and profitable monetization of a site of any size.</p>
+            <p class="pubs-main-section__text">We are pleased to collaborate with both large publishers and owners of small sites.</p>
+          </div>
+          <div class="pubs-main-section__img-block">
+            <div class="pubs-main-section__img-block-bg">
+              <img class="pubs-main-section__img-block-figure1" src="~assets/pubs/pubs-ellipse1.png" srcset="~assets/pubs/pubs-ellipse1.png 1x, ~assets/pubs/pubs-ellipse1-2x.png 2x" alt="ellipse1">
+              <img class="pubs-main-section__img-block-figure2" src="~assets/pubs/pubs-ellipse2.png" srcset="~assets/pubs/pubs-ellipse2.png 1x, ~assets/pubs/pubs-ellipse2-2x.png 2x" alt="ellipse2">
+              <img class="pubs-main-section__img-block-figure3" src="~assets/pubs/pubs-rectangle1.png" srcset="~assets/pubs/pubs-rectangle1.png 1x, ~assets/pubs/pubs-rectangle1-2x.png 2x" alt="rectangle-3">
+              <img class="pubs-main-section__img-block-figure4" src="~assets/pubs/pubs-rectangle2.svg" alt="rectangle2">
+              <img class="pubs-main-section__img-block-figure5" src="~assets/pubs/pubs-rectangle1.svg" alt="rectangle1">
+              <img class="pubs-main-section__img-block-figure6" src="~assets/pubs/pubs-ellipse.svg" alt="ellipse1">
+              <img class="pubs-main-section__img-block-figure7" src="~assets/pubs/tisers.png" srcset="~assets/pubs/tisers.png 1x, ~assets/pubs/tisers-2x.png 2x" alt="rectangle3">
+            </div>
           </div>
         </div>
+        <q-btn class="pubs-main-section__main-btn main-btn main-btn--green" @click="router.push({name: 'registration'})">Start earning with us</q-btn>
       </div>
-      <q-btn class="pubs-main-section__main-btn main-btn main-btn--green" @click="router.push({name: 'registration'})">Start earning with us</q-btn>
-    </div>
-  </section>
+    </section>
+    <section class="countries-section container">
+      <h2 class="countries-section__title title custom-font">Countries we cooperate with</h2>
+      <div>
+        <CountriesSlider/>
+      </div>
+    </section>
+
+    <section class="targeting-section targeting-section--pubs">
+      <div class="targeting-section__container container">
+        <h2 class="targeting-section__title title title--white custom-font">Configuration</h2>
+        <p class="targeting-section__intro intro intro--white">TGB • TGB In-Read</p>
+        <div class="targeting-section__desktop">
+          <TargetingSlider/>
+        </div>
+        <div class="targeting-section__mob">
+          <q-list class="targeting-section__mob-list">
+            <q-item class="targeting-section__mob-item">
+              <picture>
+                <img class="targeting-section__mob-img" src="~assets/main/begining-article.png" srcset="~assets/main/begining-article.png 1x, ~assets/main/begining-article-2x.png 2x" alt="begining article">
+              </picture>
+              <p class="targeting-section__mob-text">At the beginning of the article</p>
+            </q-item>
+            <q-item class="targeting-section__mob-item">
+              <picture>
+                <img class="targeting-section__mob-img" src="~assets/main/ending-article.png" srcset="~assets/main/ending-article.png 1x, ~assets/main/ending-article-2x.png 2x" alt="ending article">
+              </picture>
+              <p class="targeting-section__mob-text">At the end of the article</p>
+            </q-item>
+            <q-item class="targeting-section__mob-item">
+              <picture>
+                <img class="targeting-section__mob-img" src="~assets/main/middle-article.png" srcset="~assets/main/middle-article.png 1x, ~assets/main/middle-article-2x.png 2x" alt="middle article">
+              </picture>
+              <p class="targeting-section__mob-text">In the middle of the article</p>
+            </q-item>
+            <q-item class="targeting-section__mob-item">
+              <picture>
+                <img class="targeting-section__mob-img" src="~assets/main/side-article.png" srcset="~assets/main/side-article.png 1x, ~assets/main/side-article-2x.png 2x" alt="side article">
+              </picture>
+              <p class="targeting-section__mob-text">On the right sidebar</p>
+            </q-item>
+          </q-list>
+        </div>
+      </div>
+    </section>
   </q-page>
 </template>
 
 <script setup lang="ts">
+import CountriesSlider from 'src/components/CountriesSlider.vue';
+import TargetingSlider from 'components/TargetingSlider.vue';
 
 </script>
