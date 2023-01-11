babeafce904f7b2676f0dc50319df94520c6f687
jcdentonxxxxxx <jcdentonxxxxxx@gmail.com>
2023-01-11 19:03:36 +0300

    feat(pages): added pages privacy policy and terms

---
+++ src/pages/PrivacyPolicyPage.vue
@@ -0,0 +1,395 @@
+<template>
+  <div class="privacy-policy-page">
+    <h1 class="privacy-policy-page__title guest-title custom-font">
+      Privacy policy
+    </h1>
+
+    <div class="privacy-policy-page__container container">
+      <p>Last updated: 10.01.2023</p>
+      <h3 class="privacy-policy-page__subtitle">1. Who we are</h3>
+      <p>
+        1.1. Adprofex Limited company incorporated under the law of the Republic
+        of Cyprus, with registered number 435565, registered office located at
+        Themistokli Dervi, 17-19 THE CITY HOUSE, 2nd floor 1066, Nicosia, Cyprus
+        ("Adprofex").
+      </p>
+      <p>1.2. This privacy policy:</p>
+      <p>
+        provides an overview of how Adprofex Ltd collects and processes your
+        personal data, and informs you about your rights under the EU General
+        Data Protection Regulation (“GDPR”) and any national law supplementing
+        or implementing the GDPR.
+      </p>
+      <p>
+        Contains important information about, among others, the collection and
+        use of personal data; the legal grounds for the processing of that data;
+        disclosures of that personal data to third parties and the use of
+        cookies on the Website.
+      </p>
+      <p>
+        1.3. The Adprofex website (or Service) is owned and operated by Adprofex
+        (referred to in this Policy as "Adprofex", "we", "our", or "us").
+      </p>
+      <p>
+        1.4. By using the Adprofex Service, you agree to observe and be bound by
+        this Privacy Policy and you consent to the processing, collection, use,
+        and disclosure of your information and data in accordance with the terms
+        of this Privacy Policy. Please do not use the Adprofex website if you do
+        not agree to this Privacy Policy.
+      </p>
+      <p>
+        1.5. The use we make of the Information, including Personal Information,
+        collected through the website and Services may vary depending on whether
+        you are (i) a customer ("Customer"), i.e., a advertiser, partner, with
+        whom we have a contractual relationship; (ii) a visitor of our
+        Customers' websites who interacts with our Services ("User"); or (iii)
+        or a visitor of our own website ("Site Visitor"), which may include
+        Customers as well as prospective customers.
+      </p>
+      <p>
+        This Privacy Policy should be read alongside, and in addition to, our
+        Terms and conditions (accessible online at
+        <router-link to="/privacypolicy"
+          >adprofex.com/privacypolicy</router-link
+        >
+      </p>
+      <p>
+        In case of any contradictions between this Privacy Policy and the Terms,
+        this Privacy Policy will prevail.
+      </p>
+      <p>1.6. For the purposes of this privacy policy:</p>
+      <p>
+        1.6.1 when we refer to “personal data” we mean data which identifies or
+        may identify you and which may include, for example, your name, address,
+        identification number, telephone number, date of birth, occupation and
+        family status;
+      </p>
+      <p>
+        1.6.2. when we refer to “processing” we mean the handling of your
+        personal data by us, including collecting, protecting and storing your
+        personal data;
+      </p>
+      <p>
+        1.6.3. when we refer to our “Website” or our “Service” we mean
+        <router-link to="/">adprofex.com</router-link>
+      </p>
+      <h3 class="privacy-policy-page__subtitle">
+        2. Our Data Protection Principles:
+      </h3>
+      <p>
+        2.2.1. We adhere to the principles relating to processing of personal
+        data set out in the GDPR which require personal data to be:
+      </p>
+      <p>
+        2.2.2. Processed lawfully, fairly and in a transparent manner
+        (Lawfulness, Fairness and Transparency).
+      </p>
+      <p>
+        2.2.3. Collected only for specified, explicit and legitimate purposes
+        (Purpose Limitation).
+      </p>
+      <p>
+        2.2.4. Adequate, relevant and limited to what is necessary in relation
+        to the purposes for which it is Processed (Data Minimisation).
+      </p>
+      <p>2.2.5. Accurate and where necessary kept up to date (Accuracy).</p>
+      <p>
+        2.2.6. Not kept in a form which permits identification of data subjects
+        for longer than is necessary for the purposes for which the data is
+        Processed (Storage Limitation).
+      </p>
+      <p>
+        2.2.7. Processed in a manner that ensures its security using appropriate
+        technical and organisational measures to protect against unauthorised or
+        unlawful processing and against accidental loss, destruction or damage
+        (Security, Integrity and Confidentiality).
+      </p>
+      <p>
+        2.2.8. Not transferred to another country without appropriate safeguards
+        being in place (Transfer Limitation).
+      </p>
+      <p>
+        2.2.9. Made available to data subjects and data subjects allowed to
+        exercise certain rights in relation to their Personal Data (Data
+        Subject's Rights and Requests).
+      </p>
+      <p>
+        2.2.10. We are responsible for and must be able to demonstrate
+        compliance with the data protection principles listed above
+        (Accountability).
+      </p>
+      <p>
+        3.2.1 We obtain your personal data mainly through any information you
+        provide directly to us or through information provided by third parties.
+        Below is a list of ways in which we collect your personal data.
+      </p>
+      <p>Personal data collected directly from you, including:</p>
+      <ul>
+        <li>
+          when you complete our Partner or Advertiser registration forms, which
+          are available on our Website,
+        </li>
+        <li>
+          when you enter and use our Website, and/or performing contractual
+          obligations, and
+        </li>
+        <li>
+          when you contact us for any enquiries, complaints or for any other
+          reason.
+        </li>
+      </ul>
+      <h3 class="privacy-policy-page__subtitle">3. Cookie Policy</h3>
+      <p>
+        3.1. We use cookies for a variety of reasons detailed below.
+        Unfortunately in most cases there are no industry standard options for
+        disabling cookies without completely disabling the functionality and
+        features they add to this website. It is recommended that you leave on
+        all cookies if you are not sure whether you need them or not in case
+        they are used to provide a service that you use.
+      </p>
+      <p>
+        3.2. You can prevent the setting of cookies by adjusting the settings on
+        your browser (see your browser Help for how to do this). Be aware that
+        disabling cookies will affect the functionality of this and many other
+        websites that you visit. Disabling cookies will usually result in also
+        disabling certain functionality and features of this website. Therefore
+        it is recommended that you do not disable cookies.
+      </p>
+      <p>3.3. The Cookies we set</p>
+      <ul>
+        <li>
+          Account related cookies <br />
+          If you create an account with us then we will use cookies for the
+          management of the signup process and general administration. These
+          cookies will usually be deleted when you log out however in some cases
+          they may remain afterwards to remember your site preferences when
+          logged out.
+        </li>
+        <li>
+          Login related cookies <br />
+          We use cookies when you are logged in so that we can remember this
+          fact. This prevents you from having to log in every single time you
+          visit a new page. These cookies are typically removed or cleared when
+          you log out to ensure that you can only access restricted features and
+          areas when logged in.
+        </li>
+        <li>
+          Orders processing related cookies <br />
+          This site offers e-commerce or payment facilities and some cookies are
+          essential to ensure that your order is remembered between pages so
+          that we can process it properly.
+        </li>
+      </ul>
+      <p>3.4. Third Party Cookies</p>
+      <p>
+        In some special cases we also use cookies provided by trusted third
+        parties. The following section details which third party cookies you
+        might encounter through this site.
+      </p>
+
+      <ul>
+        <li>
+          Third party analytics are used to track and measure usage of this site
+          so that we can continue to produce engaging content. These cookies may
+          track things such as how long you spend on the site or pages you visit
+          which helps us to understand how we can improve the site for you.
+        </li>
+      </ul>
+
+      <h3 class="privacy-policy-page__subtitle">
+        4. Information we collect about you
+      </h3>
+      <p>
+        4.1. We set out in more detail the information we collect when you use
+        our Services, why we collect and process it and the legal bases below.
+      </p>
+
+      <!-- ToDo -->
+
+      <!-- /ToDo -->
+      <p>
+        4.2. Our legitimate interests include (1) maintaining and administrating
+        the Services; (2) providing the Services to you; (3) improving the
+        content of the Services; (4) processing of the information that you
+        choose to provide to us via email or submitting on our website or
+        Service; (5) ensuring your account is adequately protected; and (6)
+        compliance with any contractual, legal or regulatory obligations under
+        any applicable law.
+      </p>
+      <p>
+        4.3. As part of maintaining and administrating the Services we may use
+        the information to analyze user activity and ensure that rules and terms
+        of use for the Services are not violated.
+      </p>
+      <p><b>4.4. Information We May Provide to Third Parties:</b></p>
+
+      <p>
+        4.4.1. We may disclose your personal data to the third parties listed
+        below. When we do so, we require those third parties to have appropriate
+        technical and organisational measures in place to protect your personal
+        data. We will not share any of your personal data for any purpose other
+        than the purposes described in this privacy policy, nor will we sell
+        your personal data to anyone.
+      </p>
+      <p>
+        4.4.2. To our insurers and/or professional advisers insofar as
+        reasonably necessary for the purposes of managing risks, obtaining
+        professional advice, or the establishment, exercise or defence of legal
+        claims, whether in court proceedings or in an administrative or
+        out-of-court procedure.
+      </p>
+      <p>
+        4.4.3. Payment services providers we have chosen to support us with
+        financial transactions relating to our Service. We will share
+        transaction data with our payment services providers only to the extent
+        necessary for the purposes of processing your payments, refunding such
+        payments and dealing with complaints and queries relating to such
+        payments and refunds.
+      </p>
+      <p>
+        4.4.4. Governmental and regulatory bodies, including law enforcement
+        authorities, in connection with enquiries, proceedings or investigations
+        by such parties or in order to enable Propeller to comply with its legal
+        and regulatory requirements.
+      </p>
+      <p>
+        4.4.5. Partners, contractors and/or vendors in order to perform contract
+        we have with you.
+      </p>
+
+      <p><b> 4.5. Your data protection rights:</b></p>
+
+      <p>
+        4.5.1. You have the following rights in terms of the personal data we
+        hold about you:
+      </p>
+
+      <!-- ToDo2 -->
+
+      <!-- /ToDo2 -->
+
+      <h3 class="privacy-policy-page__subtitle">
+        5. International Transfers of Your Personal Data
+      </h3>
+      <p>
+        5.1. We don`t envisage that any of your personal data will be
+        transferred to countries outside the European Economic Area (EEA) as our
+        office, facilities, subcontractors and other third parties with which we
+        work are all located within the EEA.
+      </p>
+      <p>
+        5.2. In the event that this changes in the future and one or more
+        recipients of your personal data are located outside the EEA, where data
+        protection laws may be of a lower standard than in the EEA, we will
+        impose the same data protection safeguards that we deploy inside the EEA
+        to ensure that your personal data are always protected.
+      </p>
+      <h3 class="privacy-policy-page__subtitle">
+        6. Retaining and Deleting Personal Data
+      </h3>
+      <p>
+        6.1. We will keep your personal data for as long as is necessary for the
+        purpose for which we initially collected them. Once such period has
+        ended, we will keep your personal data for the longest of the following
+        periods: a) any retention period set out in our retention policy which
+        is in line with regulatory requirements relating to retention; or (b)
+        the end of the period in which legal action or investigations might
+        arise in respect of the services provided.
+      </p>
+      <p>
+        6.2. Once the retention period of your personal data lapses, we will
+        ensure that your personal data are either irreversibly anonymised or
+        securely destroyed.
+      </p>
+      <h3 class="privacy-policy-page__subtitle">
+        7. Collection and Processing of End Users Data
+      </h3>
+      <p>
+        7.1. We obtain your personal data through your browsing information
+        archive files, as informed via the cookies policies of the websites of
+        each of our Publishers (websites which show our advertising banners or
+        other ad formats). We obtain data as such: URL of the website displaying
+        our ads, referral URL, IP, country, geographical area, browser and
+        operating system, etc.
+      </p>
+      <p>
+        7.2. We may collect and process your data depending on the service we
+        provide to each partner. We may use your personal data to show ads
+        relevant to you in the context of the partner’s website you are visiting
+        at any time and with the legitimate interest to identify any possible
+        fraud of our clients.
+      </p>
+      <p>
+        7.3. We will only process your personal data when the law allows us to
+        and when expressly authorized through an agreement with our partners,
+        who have informed you of our collection of your personal data, with the
+        aim of showing ads relevant to you.
+      </p>
+      <h3 class="privacy-policy-page__subtitle">
+        8. Confidentiality and Security
+      </h3>
+      <p>
+        8.1 We deploy various security measures such as encryption and
+        authentication tools in line with the current state of the art to
+        protect and maintain the security, integrity and availability of your
+        personal data.
+      </p>
+      <p>
+        8.2 100% protection against unauthorised access in the case of data
+        transfers across the internet or a website cannot be guaranteed, but we
+        and our service providers and business partners do our utmost to protect
+        your personal data in line with the prevailing data protection
+        legislation by means of physical and electronic physical precautions.
+        Among other things, we use the following measures:
+      </p>
+      <ul>
+        <li>
+          Strict criteria for authorisation to access your personal data on a
+          “need-to-know” basis only and exclusively for the specified purpose;
+        </li>
+        <li>Transfer of acquired data in secured form;</li>
+        <li>Storage of confidential data in our secure servers;</li>
+        <li>
+          Firewall safeguarding of IT systems to provide protection against
+          unauthorised access, and
+        </li>
+        <li>
+          Continuous monitoring of access to IT systems to detect and prevent
+          the misuse of personal data.
+        </li>
+      </ul>
+      <h3 class="privacy-policy-page__subtitle">9. Changes to this Policy</h3>
+      <p>
+        9.1. We may update this privacy policy from time to time by publishing a
+        new version on our Website.
+      </p>
+      <p>
+        9.2. We will notify you appropriately when we make changes to the
+        privacy policy and we will amend its revision date so that you know when
+        we last amended it. We do however encourage you to review this statement
+        periodically so as to always be informed about how we are processing and
+        protecting your personal information.
+      </p>
+      <h3 class="privacy-policy-page__subtitle">10. Contact Us</h3>
+      <p>
+        10.1. If you have any questions, please send your inquiries to us, in
+        writing to Themistokli Dervi, 17-19 THE CITY HOUSE, 2nd floor 1066,
+        Nicosia, Cyprus or to lawyer@adprofex-ltd.com. So we can deal with your
+        enquiry effectively, please quote this Privacy Policy. We will aim to
+        respond to you within 30 days from receipt of request.
+      </p>
+      <p>
+        10.2. All correspondence received by us from you (written or electronic
+        inquiries) is classified as restricted-access information and may not be
+        disclosed without your written consent. The personal data and other
+        information about you may not be used without your consent for any
+        purpose other than for response to the inquiry, except as expressly
+        provided by law.
+      </p>
+      <p>
+        10.3. The e-mail address of our DPO is
+        <a href="mailto:lawyer@adprofex-ltd.com">lawyer@adprofex-ltd.com</a>.
+      </p>
+    </div>
+  </div>
+</template>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,4 +1,4 @@
-import {RouteRecordRaw} from 'vue-router';
+import { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
   {
@@ -8,17 +8,27 @@ const routes: RouteRecordRaw[] = [
       {
         path: '',
         name: 'main',
-        component: () => import('pages/MainPage.vue')
+        component: () => import('pages/MainPage.vue'),
       },
       {
         path: 'contacts',
         name: 'contacts',
-        component: () => import('pages/ContactsPage.vue')
+        component: () => import('pages/ContactsPage.vue'),
       },
       {
         path: 'pubs',
         name: 'pubs',
-        component: () => import('pages/PubsPage.vue')
+        component: () => import('pages/PubsPage.vue'),
+      },
+      {
+        path: 'privacypolicy',
+        name: 'privacypolicy',
+        component: () => import('pages/PrivacyPolicyPage.vue'),
+      },
+      {
+        path: 'terms&conditions',
+        name: 'terms&conditions',
+        component: () => import('pages/TermsConditionsPage.vue'),
       },
     ],
   },
@@ -27,18 +37,18 @@ const routes: RouteRecordRaw[] = [
     path: '/auth',
     component: () => import('layouts/GuestLayout.vue'),
     meta: {
-      auth: false
+      auth: false,
     },
     children: [
       {
         path: 'login',
         name: 'login',
-        component: () => import('pages/auth/LoginPage.vue')
+        component: () => import('pages/auth/LoginPage.vue'),
       },
       {
         path: 'registration',
         name: 'registration',
-        component: () => import('pages/auth/RegistrationPage.vue')
+        component: () => import('pages/auth/RegistrationPage.vue'),
       },
       // {
       //   path: 'success-register',
