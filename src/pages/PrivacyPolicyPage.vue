037ebfd2cf8b057ee0a4114826b67aeaa788359e
jcdentonxxxxxx <jcdentonxxxxxx@gmail.com>
2023-01-12 13:46:12 +0300

    feat(privacy): add tables

--- src/pages/PrivacyPolicyPage.vue
+++ src/pages/PrivacyPolicyPage.vue
@@ -49,9 +49,8 @@
       <p>
         This Privacy Policy should be read alongside, and in addition to, our
         Terms and conditions (accessible online at
-        <router-link to="/privacypolicy"
-          >adprofex.com/privacypolicy</router-link
-        >
+        <router-link to="/privacypolicy">adprofex.com/privacypolicy</router-link
+        >)
       </p>
       <p>
         In case of any contradictions between this Privacy Policy and the Terms,
@@ -205,7 +204,43 @@
       </p>
 
       <!-- ToDo -->
-
+      <br />
+      <q-table
+        :rows="rows"
+        :columns="columns"
+        row-key="name"
+        :rows-per-page-options="[0]"
+        hide-bottom
+        separator="cell"
+        :grid="$q.screen.xs"
+        :wrap-cells="true"
+        dense
+      />
+      <br />
+      <q-table
+        :rows="rowsUsers"
+        :columns="columnsUsers"
+        row-key="name"
+        :rows-per-page-options="[0]"
+        hide-bottom
+        separator="cell"
+        :grid="$q.screen.xs"
+        :wrap-cells="true"
+        dense
+      />
+      <br />
+      <q-table
+        :rows="rowsVisitors"
+        :columns="columnsVisitors"
+        row-key="name"
+        :rows-per-page-options="[0]"
+        hide-bottom
+        separator="cell"
+        :grid="$q.screen.xs"
+        :wrap-cells="true"
+        dense
+      />
+      <br />
       <!-- /ToDo -->
       <p>
         4.2. Our legitimate interests include (1) maintaining and administrating
@@ -265,7 +300,114 @@
       </p>
 
       <!-- ToDo2 -->
+      <div class="privacy-policy-protection">
+        <div class="privacy-policy-protection__wrapper">
+          <div class="privacy-policy-protection__column">
+            <b> The right to be informed </b>
+          </div>
+          <div class="privacy-policy-protection__column">
+            Adprofex Ltd is publishing this privacy policy to keep you informed
+            as to what we do with your personal data. You can ask us for
+            information regarding any data of yours that we keep at any time.
+            This information concerns, among other things, the data categories
+            we process, for what purposes we process them, the origin of the
+            data if we did not acquire them directly from you and, if
+            applicable, the recipients to whom we have sent your data.
+          </div>
+        </div>
+        <div class="privacy-policy-protection__wrapper">
+          <div class="privacy-policy-protection__column">
+            <b> The right to correction (rectification)</b>
+          </div>
+          <div class="privacy-policy-protection__column">
+            You can request that we correct your data. We will initiate
+            appropriate measures to keep the data of yours that we continuously
+            process correct, complete, and up to date, based the latest
+            information available to us.
+          </div>
+        </div>
 
+        <div class="privacy-policy-protection__wrapper">
+          <div class="privacy-policy-protection__column">
+            <b>The right to erasure</b>
+          </div>
+          <div class="privacy-policy-protection__column">
+            You can request that we delete your data provided the legal
+            requirements have been met. In accordance with Article 17 the GDPR,
+            this can be the case if:
+          </div>
+        </div>
+
+        <div class="privacy-policy-protection__wrapper">
+          <div class="privacy-policy-protection__column">
+            <b>The right to erasure </b> <br />
+            <b>(The right to be forgotten)</b>
+          </div>
+          <div class="privacy-policy-protection__column">
+            <p>
+              You can request that we delete your data provided the legal
+              requirements have been met. In accordance with Article 17 the
+              GDPR, this can be the case if:
+            </p>
+
+            <ul>
+              <li>
+                the data are no longer required for the purposes they were
+                acquired or otherwise processed;
+              </li>
+              <li>
+                you revoke your consent, which is the basis of the data
+                processing, and there is no other legal basis for the
+                processing;
+              </li>
+              <li>
+                you object to the processing of your data and there are no
+                legitimate reasons for the processing or you object to data
+                processing for the purposes of direct advertising;
+              </li>
+              <li>the data have been processed illegally.</li>
+            </ul>
+
+            <p>Where the processing is not necessary:</p>
+            <ul>
+              <li>
+                To ensure adherence to a legal obligation that requires us to
+                process your data
+              </li>
+              <li>In particular with regard to legal retention periods</li>
+              <li>To assert, exercise or defend against legal claims</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="privacy-policy-protection__wrapper">
+          <div class="privacy-policy-protection__column">
+            <b>The right to object</b>
+          </div>
+          <div class="privacy-policy-protection__column">
+            You can object to the processing of your data at any time for
+            reasons that arise from your special situation provided the data
+            processing is based on your consent or our legitimate interest or
+            that of a third party. In this case, we will no longer process your
+            data. The latter does not apply if we are able to prove there are
+            compelling, defensible reasons for the processing that outweigh your
+            interests or we require your data to assert, exercise or defend
+            against legal claims.
+          </div>
+        </div>
+
+        <div class="privacy-policy-protection__wrapper">
+          <div class="privacy-policy-protection__column">
+            <b>The right to complain</b>
+          </div>
+          <div class="privacy-policy-protection__column">
+            Adprofex Ltd takes your rights very seriously. However, if you are
+            of the opinion that we have not dealt with your complaints
+            adequately, you have the right to submit a complaint to the data
+            privacy protection authorities responsible.
+          </div>
+        </div>
+      </div>
       <!-- /ToDo2 -->
 
       <h3 class="privacy-policy-page__subtitle">
@@ -393,3 +535,174 @@
     </div>
   </div>
 </template>
+<script>
+const rows = [
+  {
+    name: 'Data you provide for registering in the Services including your email and/or mobile phone number',
+    purpose:
+      'We use this information in order to manage and administer the Services provided to you. We use this data to fulfill our obligations towards you as part of the Services (e.g. in cases where you request restoration of your account).',
+    legal: 'Legitimate interests. Performance of our contract with you',
+  },
+
+  {
+    name: 'Where necessary, a copy of your identity or  another document containing your name, surname, photograph, number of the principal identity document of you or your representative, payment details and another additional data, with that you may provide us, including through our support service. We may take additional verification steps where we consider reasonable in order to verify your account.',
+    purpose:
+      "We use this data in order to identify you, verify your account and prevent abuse and infringements of your or other persons' rights. For example, we use this information to verify your identity if you lose your credentials and wish to access your account with us.",
+    legal: 'Legitimate Interests. Performance of our contract with you',
+  },
+
+  {
+    name: 'Additional data you provide when you edit your profile page at including your first and last name, nickname, gender, date of birth, avatar picture and language preferences.',
+    purpose:
+      "We use this information in order to provide our Services to you, to manage and administer Services and as additional information to verify your account to prevent abuse and infringements of your or other persons' rights.We also use this information in order to provide you with updates and information on our and selected third parties' products and services we think you may be interested in.",
+    legal: 'Legitimate interests. Performance of our contract with you.',
+  },
+
+  {
+    name: 'Additional data received when you access the Services, including information  regarding technical devices, technical interaction with the Service such as your IP-address, time of registration in the Service, device ids, country and language settings, device model and operating system used, type of browser and your browsing behavior.',
+    purpose:
+      'We use your data for internal review in order to constantly improve the content of our Services and web pages, optimizing your user experience, to understand any errors you may encounter when using the Services, to notify you of changes to the Services and to personalise the use of our Services.',
+    legal: 'Legitimate Interests',
+  },
+
+  {
+    name: 'Information that is automatically received at the time of access to the Services with the use of cookies.',
+    purpose:
+      'Please refer to paragraph 3  which sets out the types of cookies we use and what we use these cookies for.',
+    legal: 'Consent',
+  },
+
+  {
+    name: 'Email address and contents of a message that you may submit through our website.',
+    purpose:
+      'We use this information to communicate with Customers, including via email, about the Customer’s account; to respond to Customer inquiries or complaints; to provide Customers with news, best practices or other information we think may interest them; to provide Customers with newsletters, special offers, and promotions, including via email; to contact Customers about products or services we think may interest them; and for other marketing, advertising, and promotional purposes.',
+    legal: 'Legitimate interests. Performance of our contract with you',
+  },
+
+  {
+    name: 'Information that is created by you while placing requests to our Service support',
+    purpose:
+      'We use this information in order to verify your identity and to fullfil your support request. We may also use this data in order to investigate any complaints on your behalf and to provide you with a more efficient service',
+    legal: 'Legitimate interests. Performance of our contract with you.',
+  },
+  {
+    name: 'Information that is received as the result of your using payment functionality on the Site (e.g. first and last four digits of your card number that are needed for the option of matching these details with user’s id number).',
+    purpose:
+      'We use this information in order to manage and administer the Site including providing our Site Services to you. We may also use this data in order to investigate any complaints on your behalf and to provide you with a more efficient service.',
+    legal: 'Legitimate interests. Performance of our contract with you.',
+  },
+];
+
+const columns = [
+  {
+    name: 'name',
+    required: true,
+    label: 'About Customers',
+    align: 'center',
+    field: (row) => row.name,
+    sortable: false,
+  },
+  {
+    name: 'purpose',
+    align: 'center',
+    label: 'Purpose',
+    field: 'purpose',
+    sortable: false,
+  },
+  {
+    name: 'legal',
+    align: 'center',
+    label: 'Legal Basis',
+    field: 'legal',
+    sortable: false,
+  },
+];
+
+const rowsUsers = [
+  {
+    name: "Information automatically collected  through User’s interaction with our Services through cookies and other technologies. This Information may include, without limitation, information about a User’s operating system, IP address, the webpages accessed by Users within our Customers’ websites, the link that led a User to a Customers' website, the dates and times a User accesses a Customers' website and general location information (e.g., country, city)",
+    purpose:
+      'We use this information to provide our Services to Users and our Customers. We also use this information to tailor the content and information that we may send or display to Users; to retarget content to Users through our Services and elsewhere; and to otherwise personalize your experiences while using our Services.',
+    legal: 'Legitimate interests. ',
+  },
+
+  {
+    name: 'Information that is automatically received at the time of access the Services with the use of cookies.',
+    purpose:
+      'Please refer to paragraph 3  which sets out the types of cookies we use and what we use these cookies for.',
+    legal: 'Consent',
+  },
+];
+
+const columnsUsers = [
+  {
+    name: 'name',
+    required: true,
+    label: 'About Users',
+    align: 'center',
+    field: (row) => row.name,
+    sortable: false,
+  },
+  {
+    name: 'purpose',
+    align: 'center',
+    label: 'Purpose',
+    field: 'purpose',
+    sortable: false,
+  },
+  {
+    name: 'legal',
+    align: 'center',
+    label: 'Legal Basis',
+    field: 'legal',
+    sortable: false,
+  },
+];
+
+const rowsVisitors = [
+  {
+    name: 'Information collected directly from you, when you send us an email asking a question, or submit a form on our website. We will also collect any other Information that you choose to provide to us, such as the contents of a message that you submit through our website.',
+    purpose:
+      'We use this information to provide and manage our website and Services. We also use this information to communicate with you, including via email, about our Services; to respond to your inquiries or complaints; to provide you with news, best practices or other information we think may interest you; to provide you with newsletters, special offers, and promotions, including via email; to contact you about products or services we think may interest you; and for other marketing, advertising, and promotional purposes.',
+    legal: 'Legitimate interests',
+  },
+];
+const columnsVisitors = [
+  {
+    name: 'name',
+    required: true,
+    label: 'About Site Visitors',
+    align: 'center',
+    field: (row) => row.name,
+    sortable: false,
+  },
+  {
+    name: 'purpose',
+    align: 'center',
+    label: 'Purpose',
+    field: 'purpose',
+    sortable: false,
+  },
+  {
+    name: 'legal',
+    align: 'center',
+    label: 'Legal Basis',
+    field: 'legal',
+    sortable: false,
+  },
+];
+
+export default {
+  name: 'PrivacyPolicyForm',
+  setup(props) {
+    return {
+      rows,
+      columns,
+      rowsUsers,
+      columnsUsers,
+      columnsVisitors,
+      rowsVisitors,
+    };
+  },
+};
+</script>
